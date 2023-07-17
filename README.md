5ab3ea48b294367f0576ecf806f6e417e7cef80b
kento.ishihara <ishiharakento1214@gmail.com>
2023-07-17 23:50:59 +0900

    Gameクラスのコード整形

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 -------------
 Streamの活用<br><br>list.addメソッド引数に複数要素を持つインスタンスを渡すことができました。<br>ラムダ式、メソッド参照記述してみました。<br>yearMonthをdouble型で処理しましたが、スマートではない気がしています。<br><br>
 ## 課題修正  
-*GameクラスのyearMonthのデータ型をdouble型からYearMonth型に変更
-*MainクラスのyearMonth要素の記述にYearMonth.ofメソッドを使用、result2標準出力時のデータ型変換及び小数点2桁表示を削除
+* GameクラスのyearMonthのデータ型をdouble型からYearMonth型に変更<br>
+* MainクラスのyearMonth要素の記述にYearMonth.ofメソッドを使用、result2標準出力時のデータ型変換及び小数点2桁表示を削除<br><br>
+## 課題修正2
+* Gameクラスのコードを整形
--- src/Game.java
+++ src/Game.java
@@ -1,20 +1,24 @@
 import java.time.YearMonth;
 
 public class Game {
-     private String name;
-     private String salesAgency;
-     private YearMonth yearMonth;
-     public Game(String name, String salesAgency, YearMonth yearMonth) {
+    private String name;
+    private String salesAgency;
+    private YearMonth yearMonth;
+
+    public Game(String name, String salesAgency, YearMonth yearMonth) {
          this.name = name;
          this.salesAgency = salesAgency;
-         this.yearMonth =yearMonth;
-     }
+         this.yearMonth = yearMonth;
+    }
+
     public String getName() {
         return name;
     }
-    public  String getSalesAgency() {
+
+    public String getSalesAgency() {
         return salesAgency;
     }
+
     public YearMonth getYearMonth() {
         return yearMonth;
     }
